631a7fac7631dcd64fd10b3a30b1fff8adc6b7aa
Emmie Nilsson <emmie.nilsson@gmail.com>
2020-11-03 18:26:58 +0100

    Add HTML Storybook

---
+++ storybook-html/.storybook/main.js
@@ -0,0 +1,16 @@
+module.exports = {
+  "stories": [
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ],
+  "refs": {
+    "react": {
+      "title": "Storybook React",
+      "url": "https://musing-kirch-c0df1c.netlify.app/"
+    }
+   }
+}
