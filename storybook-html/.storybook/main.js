39448411f30cb36f0cd8997b0e6da101999391ab
Emmie Nilsson <emmie.nilsson@gmail.com>
2020-12-08 15:23:32 +0100

    Button story with controls

--- storybook-html/.storybook/main.js
+++ storybook-html/.storybook/main.js
@@ -7,10 +7,4 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials"
   ],
-  "refs": {
-    "react": {
-      "title": "Storybook React",
-      "url": "https://musing-kirch-c0df1c.netlify.app/"
-    }
-   }
 }
